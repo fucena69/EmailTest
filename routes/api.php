7f86dffe9ea8c740af60dc46ba2067d7b299f74b
fucena69 <cristoffer.massive@gmail.com>
2024-07-14 14:42:14 +0800

    track debug

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Email;
 
-Route::get('/user', function (Request $request) {
+Route::get('/email', function (Request $request) {
   //  return $request->user();
   [Email::class,"sendWelcomeEmail"];
-  return "asdasdasd";
+  return "testttt";
 });
 
 
